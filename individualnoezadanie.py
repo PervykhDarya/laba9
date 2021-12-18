0f5a4c9eff4859af88fbebda99e3eec9c550cb78
PervykhDarya <90053137+PervykhDarya@users.noreply.github.com>
2021-12-18 14:20:21 +0300

    исправила idz

--- individualnoezadanie.py
+++ individualnoezadanie.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         elif command == 'add':
             name = input("Название продукта: ")
             shop = input("Название магазина: ")
-            price = input("Стоимость: ")
+            price = float(input("Стоимость: "))
 
             product = {
                 'name': name,
